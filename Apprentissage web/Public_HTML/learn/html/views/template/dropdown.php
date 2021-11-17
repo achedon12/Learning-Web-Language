80e069eabbd9d42ba3c75fbce3e25b4a668c4081
leoderoin <leoderoinleo@gmail.com>
2021-11-17 07:25:44 +0100

    update

---
+++ Apprentissage web/Public_HTML/learn/html/views/template/dropdown.php
@@ -0,0 +1,30 @@
+<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="topnav" id="myTopnav">
+            <a href="../../../../index.html" class="active">Home <i class="fa fa-home"></i></a>
+            <a href="#base">Base d'un site</a>
+            <div class="dropdown">
+                <button class="dropbtn">Balises importantes 
+                  <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                  <a href="../dropdown/p.html" title="Tp pdf">Paragraphe</a>
+                  <a href="../dropdown/titre.html" title="Balise Titre">titre</a>
+                  <a href="#meta" title="Balise Meta">meta</a>
+                  <a href="../dropdown/nav.html" title="Balise Navigation">navigation</a>
+                  <a href="#footer" title="Balise Footer">footer</a>
+                  <a href="#article" title="Balise Article">article</a>
+                  <a href="../dropdown/liste.html" title="Balise Liste">Liste</a>
+                  <a href="../dropdown/form.html" title="Balise Formulaire">Formulaire</a>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
